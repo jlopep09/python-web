021f01f542f9fc2a2a855d8df26db2874328d525
jlopep09 <jlopep09@estudiantes.unileon.es>
2024-10-27 08:48:16 +0100

    added comment

--- link_bio/link_bio/utils.py
+++ link_bio/link_bio/utils.py
@@ -1,11 +1,6 @@
-import reflex as rx
-from link_bio.components.navbar import navbar
-from link_bio.views.header import header
-from link_bio.views.links.links import links, linksExtra
-from link_bio.views.footer import footer
-import link_bio.styles.styles as styles
-from link_bio.components.title import title
-
+"""
+Metadatos de las páginas
+"""
 
 index_title = "ChispyDev todos los links relevantes"
 index_description = "Con estos links encontrarás toda la información relevante sobre mi"
