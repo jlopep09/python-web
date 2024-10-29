4d4bb422dae4891d3253825826e8eda533c2cd4b
jlopep09 <jlopep09@estudiantes.unileon.es>
2024-10-29 08:51:33 +0100

    Fixed crash, the imports are needed by reflex

--- link_bio/link_bio/link_bio.py
+++ link_bio/link_bio/link_bio.py
@@ -1,5 +1,9 @@
 import reflex as rx
 import link_bio.styles.styles as styles
+#Imports para que el router automatico pueda encontrar las paginas
+import link_bio.pages.index as links_page
+import link_bio.pages.courses as courses_page
+
 
 
 app = rx.App(
@@ -8,7 +12,5 @@ app = rx.App(
         "https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
     ]
 )
-
-
-
+#app.add_page(index)
 app._compile()
