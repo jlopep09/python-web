a09982ade457278d6cbe41a05222b716e5ac2506
jlopep09 <jlopep09@estudiantes.unileon.es>
2024-10-30 18:18:51 +0100

    implemented get users methods

--- tutorial_backend/FastAPI/main.py
+++ tutorial_backend/FastAPI/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+import user
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello FastApi!"}
+@app.get("/users")
+async def getUserInfo():
+    return user.user_list
+
+@app.get("/users/{id}")
+async def getUserInfo(id: int):
+    return list(filter(lambda user: user.id == id, user.user_list))
+
